13136cc12cbe9c232ace0589b52f9a0faa737d08
Elizabeth <ms.lilibeth@yandex.ru>
2016-04-03 23:49:48 +0500

    getting proper urls using regular expression

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import pycurl
 from io import BytesIO
 from bs4 import BeautifulSoup
+import re
 
 
 buffer = BytesIO()
@@ -12,7 +13,14 @@ c.close()
 body = buffer.getvalue()
 html_doc = body.decode('iso-8859-1')
 soup = BeautifulSoup(html_doc, 'html.parser')
-metas = soup.find_all('meta itemprop=\"url\"')
+metas = soup.find_all('meta')
+exp = re.compile('https.+id.')
+metas_suitable = []
 for link in metas:
-    print(link.get('content'))
+    if exp.match(link.get('content')):
+        metas_suitable.append(link.get('content'))
+
+for link in metas_suitable:
+    print(link)
+    print('-------------------')
 
