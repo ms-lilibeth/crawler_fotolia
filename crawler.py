315ceaad0c92a22627bcbd39bc6ba408b62ea25a
Elizabeth <ms.lilibeth@yandex.ru>
2016-04-04 01:06:22 +0500

    Now able to check whether the page is about the photo or video

--- crawler.py
+++ crawler.py
@@ -3,24 +3,61 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 import re
 
+def GetParsedHtml(link):
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, link)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    body = buffer.getvalue()
+    html_doc = body.decode('iso-8859-1')
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    return soup
 
-buffer = BytesIO()
-c = pycurl.Curl()
-c.setopt(c.URL, 'https://us.fotolia.com/p/202938145')
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
-c.close()
-body = buffer.getvalue()
-html_doc = body.decode('iso-8859-1')
-soup = BeautifulSoup(html_doc, 'html.parser')
-metas = soup.find_all('meta')
-exp = re.compile('https.+id.')
-metas_suitable = []
-for link in metas:
-    if exp.match(link.get('content')):
-        metas_suitable.append(link.get('content'))
+def IsPhoto(page):
+    if not type(page) is BeautifulSoup:
+        parsed_html = GetParsedHtml(page)
+    else:
+        parsed_html = page
+    dt_tags = parsed_html.find_all("dt")
+    exp_photo = re.compile(".*photo reference.*", re.IGNORECASE)
+    exp_video = re.compile(".*video reference.*", re.IGNORECASE)
+    for tag in dt_tags:
+        if exp_photo.match(tag.text):
+            return True
+        if exp_video.match(tag.text):
+            return False
+    raise Exception("Cannot identify which type of content it is")
 
-for link in metas_suitable:
-    print(link)
-    print('-------------------')
+def GetAllPhotoLinksOnPage(page):
+    if not type(page) is BeautifulSoup:
+        parsed_html = GetParsedHtml(page)
+    else:
+        parsed_html = page
+    metas = parsed_html.find_all('meta')
+    exp = re.compile('https.+id.')
+    metas_suitable = []
+    for link in metas:
+        if exp.match(link.get('content')):
+            metas_suitable.append(link.get('content'))
+    return metas_suitable
+
+
+def CollectData_Video(page):
+    # still not ready!
+    if not type(page) is BeautifulSoup:
+        parsed_html = GetParsedHtml(page)
+    else:
+        parsed_html = page
+
+def CollectData_Photo(page):
+    # still not ready!
+    if not type(page) is BeautifulSoup:
+        parsed_html = GetParsedHtml(page)
+    else:
+        parsed_html = page
+
+
+print(IsPhoto('https://us.fotolia.com/id/106035423'))
 
